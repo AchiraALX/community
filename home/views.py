6d1f9b209f00079fa6da73fa397676c26eddd218
AchiraALX <ed127720@students.mu.ac.ke>
2022-09-18 13:54:45 +0300

    Created the community app

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+
+
+def index(request):
+    return render(request, 'index.html')
 
 # Create your views here.
